137ae7b51680480432d1deab8807000314765f82
Алексей Поддубный <alkalineal54@gmail.com>
2022-06-12 00:41:59 +0300

    change using in shop repository

--- PriceProject/DAL/Repositories/ShopRepository.cs
+++ PriceProject/DAL/Repositories/ShopRepository.cs
@@ -17,47 +17,37 @@ namespace PriceProject.DAL.Repositories
 
         public Shop Create(Shop shop)
         {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                var sqlQuery = "INSERT INTO SHOPS (NAME, COORDINATES, DESCRIPTION) VALUES(@NAME, @Coordinates, @Description); SELECT CAST(SCOPE_IDENTITY() as int)";
-                int? shopId = db.Query<int>(sqlQuery, shop).FirstOrDefault();
-                shop.ID = shopId.Value;
-                return shop;
-            }
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var sqlQuery = "INSERT INTO SHOPS (NAME, COORDINATES, DESCRIPTION) VALUES(@NAME, @Coordinates, @Description); SELECT CAST(SCOPE_IDENTITY() as int)";
+            int? shopId = db.Query<int>(sqlQuery, shop).FirstOrDefault();
+            shop.ID = shopId.Value;
+            return shop;
         }
 
         public void Delete(int id)
         {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                var sqlQuery = "DELETE FROM SHOPS WHERE ID = @id";
-                db.Execute(sqlQuery, new { id });
-            }
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var sqlQuery = "DELETE FROM SHOPS WHERE ID = @id";
+            db.Execute(sqlQuery, new { id });
         }
 
         public Shop Get(int id)
         {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                return db.Query<Shop>("SELECT * FROM SHOPS WHERE ID = @id", new { id }).FirstOrDefault();
-            }
+            using IDbConnection db = new SqlConnection(_connectionString);
+            return db.Query<Shop>("SELECT * FROM SHOPS WHERE ID = @id", new { id }).FirstOrDefault();
         }
 
         public List<Shop> GetShops()
         {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                return db.Query<Shop>("SELECT * FROM SHOPS").ToList();
-            }
+            using IDbConnection db = new SqlConnection(_connectionString);
+            return db.Query<Shop>("SELECT * FROM SHOPS").ToList();
         }
 
         public void Update(Shop shop)
         {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                var sqlQuery = "UPDATE Users SET NAME = @NAME, COORDINATES = @Coordinates, DESCRIPTION = @Description";
-                db.Execute(sqlQuery, shop);
-            }
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var sqlQuery = "UPDATE Users SET NAME = @NAME, COORDINATES = @Coordinates, DESCRIPTION = @Description";
+            db.Execute(sqlQuery, shop);
         }
     }
 }
